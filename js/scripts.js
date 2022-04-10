bd9253b9a55ea3ce0adbbb563aa18576216b28f4
nadya-arinich <97697219+nadya-arinich@users.noreply.github.com>
2022-04-10 16:38:23 +0300

    Add files via upload

--- js/scripts.js
+++ js/scripts.js
@@ -1,26 +1,24 @@
-var modal = document.getElementById("myModal");
-var btn = document.getElementById("myBtn");
-var span = document.getElementsByClassName("close")[0];
-
-for (var i = 0; i < btn.length; i++) {
-  btn[i].onclick = function() {
-    var id = this.getAttribute('data-modal');
-    var modal = document.getElementById(id);
-    modal.style.display = 'block';
-  }
-}
-
-for (var i = 0; i < close.length; i++) {
-  close[i].onclick = function() {
-    var id = this.getAttribute('data-modal');
-    var modal = document.getElementById(id);
-    modal.style.display = 'none';
-  }
-}
-
-window.onclick = function(event) {
-  if (event.target == modal) {
-    modal.style.display = "none";
-  }
-}
-
+$(document).ready(function($) {
+	$('.popup-open').click(function() {
+		$('.popup-fade').fadeIn();
+		return false;
+	});	
+	
+	$('.popup-close').click(function() {
+		$(this).parents('.popup-fade').fadeOut();
+		return false;
+	});		
+ 
+	$(document).keydown(function(e) {
+		if (e.keyCode === 27) {
+			e.stopPropagation();
+			$('.popup-fade').fadeOut();
+		}
+	});
+	
+	$('.popup-fade').click(function(e) {
+		if ($(e.target).closest('.popup').length == 0) {
+			$(this).fadeOut();					
+		}
+	});
+});
